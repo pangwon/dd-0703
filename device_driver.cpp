b383eeb948ef88e9c7086d53f5806977a896e7cd
Kwangwon Min <kwangwon.min@gmail.com>
2025-07-03 16:21:46 +0900

    Implement mission 1 - read, write in d/d and testing with mocking

--- device_driver.cpp
+++ device_driver.cpp
@@ -1,16 +1,29 @@
 #include "device_driver.h"
+#include <exception>
+
+class ReadFailException : public std::exception {};
+class WriteFailException : public std::exception {};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
+    int ret = (int)m_hardware->read(address);
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    for (int i = 0; i < 4; i++) {
+        int tmp = (int)m_hardware->read(address);
+        if (ret != tmp && i != 0)
+            throw ReadFailException();
+    }
+    return ret;
 }
 
 void DeviceDriver::write(long address, int data)
 {
     // TODO: implement this method
+    if (m_hardware->read(address) != 0xFF)
+        throw WriteFailException();
+
     m_hardware->write(address, (unsigned char)data);
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,61 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
+#include <stdexcept>
 
-TEST(DeviceDriver, ReadFromHW) {
+using namespace testing;
+
+class MockHardware : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class DeviceDriverFixture : public Test {
+public:
+	MockHardware HARDWARE;
+	DeviceDriver driver{ &HARDWARE };
+};
+
+TEST_F(DeviceDriverFixture, ReadFromHW) {
 	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
+	// Read 5 times
+	EXPECT_CALL(HARDWARE, read)
+		.Times(5)
+		.WillRepeatedly(Return(0xAA));
 	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
+	EXPECT_EQ(0xAA, data);
+}
+
+TEST_F(DeviceDriverFixture, ReadFailTest) {
+	// TODO : replace hardware with a Test Double
+	//// 결과 다를 때 exception
+	// 
+	EXPECT_CALL(HARDWARE, read)
+		.WillOnce(Return(0xAA))
+		.WillOnce(Return(0xAA))
+		.WillOnce(Return(0xAA))
+		.WillOnce(Return(0xAA))
+		.WillOnce(Return(0xAB));
+
+	EXPECT_THROW(driver.read(0xAA), std::exception);
+}
+
+TEST_F(DeviceDriverFixture, WriteToHW) {
+	// TODO : replace hardware with a Test Double
+	EXPECT_CALL(HARDWARE, read(0xAA))
+		.WillRepeatedly(Return(0xFF));
+	driver.write(0xAA, 0xBB);
+	EXPECT_CALL(HARDWARE, read(0xAA))
+		.WillRepeatedly(Return(0xBB));
+	EXPECT_EQ(0xBB, driver.read(0xAA));
+}
+
+TEST_F(DeviceDriverFixture, WriteFailTest) {
+	EXPECT_CALL(HARDWARE, read(0xAA))
+		.WillRepeatedly(Return(0xFA));
+
+	EXPECT_THROW(driver.write(0xAA, 0xBB), std::exception);
+
 }
 
 int main() {
