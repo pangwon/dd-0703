73967ac443dd878ea77d65005fc974d372d62936
Kwangwon Min <kwangwon.min@gmail.com>
2025-07-03 16:36:11 +0900

    Refactor

--- main.cpp
+++ main.cpp
@@ -17,19 +17,14 @@ public:
 };
 
 TEST_F(DeviceDriverFixture, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	// Read 5 times
 	EXPECT_CALL(HARDWARE, read)
 		.Times(5)
 		.WillRepeatedly(Return(0xAA));
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0xAA, data);
+
+	EXPECT_EQ(0xAA, driver.read(0xAA));
 }
 
 TEST_F(DeviceDriverFixture, ReadFailTest) {
-	// TODO : replace hardware with a Test Double
-	//// 결과 다를 때 exception
-	// 
 	EXPECT_CALL(HARDWARE, read)
 		.WillOnce(Return(0xAA))
 		.WillOnce(Return(0xAA))
@@ -41,21 +36,18 @@ TEST_F(DeviceDriverFixture, ReadFailTest) {
 }
 
 TEST_F(DeviceDriverFixture, WriteToHW) {
-	// TODO : replace hardware with a Test Double
 	EXPECT_CALL(HARDWARE, read(0xAA))
+		.Times(1)
 		.WillRepeatedly(Return(0xFF));
+
 	driver.write(0xAA, 0xBB);
-	EXPECT_CALL(HARDWARE, read(0xAA))
-		.WillRepeatedly(Return(0xBB));
-	EXPECT_EQ(0xBB, driver.read(0xAA));
 }
 
 TEST_F(DeviceDriverFixture, WriteFailTest) {
 	EXPECT_CALL(HARDWARE, read(0xAA))
-		.WillRepeatedly(Return(0xFA));
+		.WillOnce(Return(0xFA));
 
 	EXPECT_THROW(driver.write(0xAA, 0xBB), std::exception);
-
 }
 
 int main() {
